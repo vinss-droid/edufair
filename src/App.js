0f5cf4038da27acebd647f733ffe3c134d25e7aa
vinss-droid <kevinsipahutar220604@gmail.com>
2023-07-03 09:22:18 +0700

    adding countdown regristration silogy

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import {isAuthenticated} from "./helper/Auth";
 import {useQueryURL} from "./Utils/Utils";
 import ShortLink from "./pages/ShortLink";
 import {Analytics} from "@vercel/analytics/react";
+import Countdown from "./components/event/silogy/Countdown";
 
 function App() {
 
@@ -55,17 +56,18 @@ function App() {
       <Router>
           <div className="App select-none">
               <Analytics />
-              {/*<Navbar />*/}
+              <Navbar />
               <Routes>
-                  <Route exact path="*" element={<NotFound />} />
-                  {/*<Route exact path="/" element={<Home />} />*/}
+                  <Route exact path="/" element={<Home />} />
                   <Route exact path="/workshop-pbb" element={<ShortLink to="/event/workshop/daftar" />} />
-                  {/*<Route exact path="/event/webinar" element={<Webinar />} />*/}
-                  {/*<Route exact path="/event/workshop" element={<Workshop />} />*/}
-                  {/*<Route exact path="/event/silogy-expo" element={<SilogyExpo />} />*/}
-                  {/*<Route exact path="/event/silogy-expo/lomba" element={<Contest />} />*/}
-                  {/*<Route exact path="/event/silogy-expo/talkshow" element={<Talkshow />} />*/}
-                  {/*<Route exact path="/event/silogy-expo/pameran" element={<Exhibition />} />*/}
+                  <Route exact path="/silogy-expo" element={<Countdown />}/>
+                  <Route exact path="/event/webinar" element={<Webinar />} />
+                  <Route exact path="/event/workshop" element={<Workshop />} />
+                  <Route exact path="/event/silogy-expo" element={<SilogyExpo />} />
+                  <Route exact path="/event/silogy-expo/lomba" element={<Contest />} />
+                  <Route exact path="/event/silogy-expo/talkshow" element={<Talkshow />} />
+                  <Route exact path="/event/silogy-expo/pameran" element={<Exhibition />} />
+                  <Route exact path="*" element={<NotFound />} />
 
                   {/*Redirect After Login*/}
                   <Route exact path="/auth/login" element={
@@ -75,11 +77,11 @@ function App() {
                   } />
 
                   {/*Private Route*/}
-                  {/*<Route exact path="/event/webinar/daftar" element={*/}
-                  {/*    <PrivateRoute>*/}
-                  {/*        <RegisterWebinar />*/}
-                  {/*    </PrivateRoute>*/}
-                  {/*} />*/}
+                  <Route exact path="/event/webinar/daftar" element={
+                      <PrivateRoute>
+                          <RegisterWebinar />
+                      </PrivateRoute>
+                  } />
                   <Route exact path="/event/workshop/daftar" element={
                       <PrivateRoute>
                           <RegisterWorkshop />
---
+++ src/components/event/silogy/Countdown.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import EdufairLogo from "../../../img/logo/logo-edufair-2023.png";
+
+const Countdown = () => {
+
+    const calculateTimeLeft = () => {
+        const difference = +new Date("July 05, 2023 00:00:00 GMT+07:00") - +new Date().getTime();
+        let timeLeft = {};
+
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+                minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+                seconds: Math.floor((difference % (1000 * 60)) / 1000)
+            };
+        }
+
+        return timeLeft;
+    };
+
+    const [timeLeft, setTimeLeft] = React.useState(calculateTimeLeft());
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+    });
+
+    return (
+        <div className="bg-[#1B254B] min-h-screen flex justify-center items-center rounded-md">
+            <div className="w-6/12 my-4 bg-white/95 px-5 rounded-md shadow-md py-8 max-[992px]:w-full max-[992px]:mx-4 max-[992px]:px-5 text-center">
+                <div>
+                    <img src={EdufairLogo} alt="webinar" className="mx-auto" width="25%" />
+                    <h4 className="text-4xl text-center font-bold mt-6 uppercase">
+                        SILOGY EXPO 2023
+                    </h4>
+                    <hr className="my-4"/>
+                    <h3 className="text-2xl text-center font-bold uppercase">
+                        Pendaftaran akan dibuka dalam
+                    </h3>
+                    <h2 className="text-2xl text-center font-semibold mt-5 uppercase text-red-600">
+                        {timeLeft.days || timeLeft.hours || timeLeft.minutes || timeLeft.seconds ? (
+                            <p>
+                                <span>{timeLeft.days}</span>
+                                <span className="mr-3"> Hari </span>
+                                <span>{timeLeft.hours}</span>
+                                <span className="mr-3"> Jam </span>
+                                <span>{timeLeft.minutes}</span>
+                                <span className="mr-3"> Menit </span>
+                                <span>{timeLeft.seconds}</span>
+                                <span> Detik </span>
+                            </p>
+                        ) : (
+                            <p>Time is up 🔥</p>
+                        )}
+                    </h2>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Countdown;
