f299cb92c33c16854537bf52840f7e505552648f
vinss-droid <kevinsipahutar220604@gmail.com>
2023-07-16 20:44:46 +0700

    changing form field register silogy

--- src/components/event/silogy/contest/TimelineContest.js
+++ src/components/event/silogy/contest/TimelineContest.js
@@ -6,27 +6,28 @@ const TimelineContest = () => {
         <ol className="relative border-l border-gray-200 dark:border-gray-700 mb-24 mt-16">
             <li className="mb-10 ml-6">
                 <span
-                    className="absolute flex items-center justify-center w-6 h-6 bg-red-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                    <svg aria-hidden="true" className="w-3 h-3 text-red-800 dark:text-blue-300" fill="currentColor"
+                    className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                    <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor"
                          viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" cliplRule="evenodd"></path>
                     </svg>
                 </span>
                 <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
                     Pendaftaran & Pengumpulan Karya
-                    <span className="bg-red-100 text-red-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3 max-[768px]:text-center">
-                        Segera Dilaksanakan
+                    <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3 max-[768px]:text-center">
+                        Sedang Dilaksanakan
                     </span>
                 </h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Dilaksanakan Pada <br className="md:hidden" /> 01 Juli 2023 - 05 Agustus 2023
+                    Dilaksanakan Pada <br className="md:hidden" /> 11 Juli 2023 - 11 Agustus 2023
                 </time>
                 <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
                     Para calon peserta yang akan mengikuti Lomba diwajibkan untuk mendaftar pada tombol dibawah!
                 </p>
-                <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-600  rounded-lg hover:bg-blue-700 hover:text-gray-100 focus:z-10 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700 cursor-pointer disabled:bg-blue-400 disabled:cursor-not-allowed" disabled>
+                <Link to="/silogy-expo"
+                    className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-600  rounded-lg hover:bg-blue-700 hover:text-gray-100 focus:z-10 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700 cursor-pointer disabled:bg-blue-400 disabled:cursor-not-allowed" disabled>
                     Daftar Lomba
-                </button>
+                </Link>
             </li>
             <li className="mb-10 ml-6">
                 <span
@@ -43,10 +44,10 @@ const TimelineContest = () => {
                     {/*</span>*/}
                 </h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Dilaksanakan Pada <br className="md:hidden" /> 06 Agustus 2023 - 11 Agustus 2023
+                    Dilaksanakan Pada <br className="md:hidden" /> 12 Agustus 2023 - 15 Agustus 2023
                 </time>
                 <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-                    Seluruh karya peserta yang sudah dikumpulkan akan diseleksi oleh panitia.
+                    Tahap seleksi karya oleh panitia.
                 </p>
             </li>
             <li className="mb-10 ml-6">
@@ -64,7 +65,7 @@ const TimelineContest = () => {
                     {/*</span>*/}
                 </h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Dilaksanakan Pada <br className="md:hidden" /> 12 Agustus 2023
+                    Dilaksanakan Pada <br className="md:hidden" /> 16 Agustus 2023
                 </time>
                 <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
                     Pengumuman akan diumumkan di grup WhatsApp.
@@ -85,7 +86,7 @@ const TimelineContest = () => {
                     {/*</span>*/}
                 </h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Dilaksanakan Pada <br className="md:hidden" /> 14 Agustus 2023
+                    Dilaksanakan Pada <br className="md:hidden" /> 18 Agustus 2023
                 </time>
                 <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
                     Peserta yang lolos tahap seleksi akan mengadakan Technical Meeting dengan panitia.
@@ -106,7 +107,7 @@ const TimelineContest = () => {
                     {/*</span>*/}
                 </h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Dilaksanakan Pada <br className="md:hidden" /> 15 Agustus 2023 - 20 Agustus 2023
+                    Dilaksanakan Pada <br className="md:hidden" /> 19 Agustus 2023 - 21 Agustus 2023
                 </time>
                 <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
                     Peserta menyiapkan bahan presentasi serta membuat bahan presentasi dalam bentuk PPT.
@@ -127,7 +128,7 @@ const TimelineContest = () => {
                     {/*</span>*/}
                 </h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Dilaksanakan Pada <br className="md:hidden" /> 21 Agustus 2023
+                    Dilaksanakan Pada <br className="md:hidden" /> 22 Agustus 2023
                 </time>
                 <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
                     Peserta akan presentasi PPT serta Karya yang telah dibuat kepada Juri.
--- src/components/event/silogy/register/FormRegisterSilogy.js
+++ src/components/event/silogy/register/FormRegisterSilogy.js
@@ -33,7 +33,6 @@ const FormRegisterSilogy = (props) => {
     // Form Data
     const [data, setData] = React.useState({
         type: type,
-        name: user.name
     })
 
     // Form Data Team Member
@@ -162,13 +161,12 @@ const FormRegisterSilogy = (props) => {
         <form autoComplete="off" onSubmit={validateData}>
             <div className="mb-6">
                 <label htmlFor="full_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Nama Lengkap <span className="text-red-600">(Untuk Sertifikat)</span>
+                    Nama Tim
                 </label>
                 <input type="text" id="full_name"
                        className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 disabled:cursor-not-allowed disabled:bg-gray-100"
-                       placeholder="Nama Lengkap"
+                       placeholder="Nama Tim"
                        required
-                       value={data.name}
                        disabled={isLoading}
                        onChange={(e) => setData({...data, name: e.target.value})}
                 />
